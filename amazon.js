edbe2b1e227608df1745782169732e3ea394fc12
crazyquark <cristian.sandu@gmail.com>
2016-08-31 15:50:40 +0300

    amazon

---
+++ amazon.js
@@ -0,0 +1,18 @@
+let {
+    AmazonClient,
+    PrestigeClient
+} = require('shipit');
+
+amazonClient = new AmazonClient();
+
+orderID = '106-9151392-7203433';
+orderingShipmentId = 'DmZd0KS8k';
+amazonClient.requestData({orderID, orderingShipmentId}, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+
+    if (result) {
+        console.log(result);
+    }
+});
