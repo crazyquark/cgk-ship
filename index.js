5cb2f475efee7d4c65f421e051d54cb941940a5c
crazyquark <cristian.sandu@gmail.com>
2016-08-31 15:47:28 +0300

    test

--- index.js
+++ index.js
@@ -18,37 +18,42 @@ var ups = new UpsClient({
     password: 'shhh-secret',
 });
 
-fedex = new FedexClient({
-  key: 'xyxyxyxyabababab',
-  password: 'asdfawasfdasdfasdf1',
-  account: '123456789',
-  meter: '99999999',
+// fedex = new FedexClient({
+//   key: 'xyxyxyxyabababab',
+//   password: 'asdfawasfdasdfasdf1',
+//   account: '123456789',
+//   meter: '99999999',
+// });
+
+// usps = new UspsClient({
+//   userId: '590XABCR3210',
+//   clientIp: '10.5.5.1',
+// });
+
+// lsClient = new LasershipClient();
+
+// dhlClient = new DhlClient({
+//   userId: 'SHIPI_79999',
+//   password: 'shipit',
+// });
+
+// dhlgmClient = new DhlGmClient();
+
+// canadaPostClient: new CanadaPostClient({
+//   username: 'maple-leafs',
+//   password: 'zamboni',
+// });
+
+// onTrac = new OnTracClient();
+
+// upsmi = new UpsMiClient();
+
+// amazonClient = new AmazonClient();
+
+// prestige = new PrestigeClient();
+
+ups.requestData({trackingNumber: '1Z1234567890123456'}, (err, result) => {
+    console.log(err);
+    console.log(result);
 });
-
-usps = new UspsClient({
-  userId: '590XABCR3210',
-  clientIp: '10.5.5.1',
-});
-
-lsClient = new LasershipClient();
-
-dhlClient = new DhlClient({
-  userId: 'SHIPI_79999',
-  password: 'shipit',
-});
-
-dhlgmClient = new DhlGmClient();
-
-canadaPostClient: new CanadaPostClient({
-  username: 'maple-leafs',
-  password: 'zamboni',
-});
-
-onTrac = new OnTracClient();
-
-upsmi = new UpsMiClient();
-
-amazonClient = new AmazonClient();
-
-prestige = new PrestigeClient();
 
