3a4d00bdd13c37e3ab54064a3092c6ff291c13a0
crazyquark <cristian.sandu@gmail.com>
2016-09-01 12:29:12 +0300

    small express js app

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var Guess = require('./lib/guess.js');
+
+app.get('/guessCarriers', (req, res) => {
+  let trackingID = req.query.trackingID;
+
+  res.send({
+    carriers: Guess.guessCarriers(trackingID)
+  });
+});
+
+app.get('/track' , (req, res) => {
+  let carrier = req.query.carrier;
+  let trackingID = req.query.trackingID;
+  
+  res.send({
+    info: 'N/A'
+  });
 });
 
 app.listen(3000, function () {
