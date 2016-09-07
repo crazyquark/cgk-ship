cf467f6e7dd25005fb8b502005ad3c6f650ecdf7
crazyquark <cristian.sandu@gmail.com>
2016-09-07 15:09:12 +0300

    more stuff

---
+++ public/client.js
@@ -0,0 +1,44 @@
+var trackingApp = angular.module('trackingApp', []);
+
+function mainController($scope, $http) {
+    $scope.availableCarriers = [
+        {
+            name: '--autodetect--',
+            value: null,
+        },
+        {
+            name: 'UPS',
+            value: 'ups',
+        },
+    ];
+
+    $scope.carrier = $scope.availableCarriers[0];
+
+    $scope.track = function (form) {
+        if (!form.$valid) {
+            return;
+        }
+
+        if ($scope.trackingNumber) {
+            var trackingUrl = '/api/track?trackingID=' + $scope.trackingNumber;
+
+            if ($scope.carrier.value !== null) {
+                trackingUrl += '&carrier=' + $scope.carrier.value;
+            }
+
+            $http.get(trackingUrl).then(function (res) {
+                var data = res.data;
+
+                console.log(data);
+
+                if (data.error) {
+                    swal('No go', 'There was an error: ' + data.error, 'error');
+                } else {
+                    swal('Here\'s your data:', JSON.stringify(data)) ;
+                }
+            }, function (err) {
+                console.log(err);
+            });
+        }
+    };
+}
